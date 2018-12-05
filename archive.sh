59550889a904f3cefadcd5e47e6ddc16d261eae0
Huzaifa <huzaifah.mustafa@gmail.com>
2018-12-05 13:26:40 +0500

    build script for packaging repository

--- archive.sh
+++ archive.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 
-SHOPWARE_DIRECTORY=../../../../shopware
+# Get current directory of the script
+ROOT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+# Change permissions to be able to create directories
+chmod 777 ${ROOT_DIR}
+
+# Create tmp directory before copying as it can cause no directory found error
+mkdir -p "${ROOT_DIR}/tmp/FinSearchUnified/"
+
+# Extract version from the plugin.xml file
 VERSION_RAW="$(sed -n 's|<version>\(.*\)</version>|\1|p' ./FinSearchUnified/plugin.xml)"
 
+# Trim the whitespaces from the version otherwise it would cause problems
+# in creating the archive zip file
 VERSION="$(echo -e "${VERSION_RAW}" | tr -d '[:space:]')"
-
 echo "Version: ${VERSION}"
 
-cp -rvf ./FinSearchUnified/ ${SHOPWARE_DIRECTORY}/tmp
-
+# Copying plugins files
+cp -rvf ./FinSearchUnified/ ${ROOT_DIR}/tmp/FinSearchUnified
 echo "Copying files ... "
 
-cd FinSearchUnified
+# Get into the created directory for running the archive command
+cd "${ROOT_DIR}/tmp/FinSearchUnified/"
 
+# Install dependencies
 composer install --no-dev
-composer archive --format=zip --file=FinSearchUnified-${VERSION} --dir=${SHOPWARE_DIRECTORY}
 
-rm -rf ${SHOPWARE_DIRECTORY}/tmp
+# Run archive command to create the zip in the root directory
+composer archive --format=zip --file=FinSearchUnified-${VERSION} --dir=${ROOT_DIR}
+
+# Delete the directory after script execution
+rm -rf "${ROOT_DIR}/tmp"
