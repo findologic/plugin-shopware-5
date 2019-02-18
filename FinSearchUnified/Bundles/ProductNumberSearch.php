f66e7f28b167dd8b3a09b0c1f0b7dde49fef4dac
Ismail Hanli <i.hanli@findologic.com>
2019-02-18 11:15:05 +0100

    Add compatibility layer

---
+++ FinSearchUnified/Bundles/ProductNumberSearch.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FinSearchUnified\Bundles;
+
+/**
+ * @deprecated No longer used by internal code and not recommended.
+ * @see FinSearchUnified\Bundle\ProductNumberSearch
+ */
+class ProductNumberSearch extends \FinSearchUnified\Bundle\ProductNumberSearch
+{}
