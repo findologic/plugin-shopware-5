b1df7ad560bdf831b1e8d5080341d5668c3f9766
Huzaifa <huzaifah.mustafa@gmail.com>
2019-03-01 19:04:52 +0500

    Refactored code

--- FinSearchUnified/Tests/Bundle/SearchBundleFindologic/QueryBuilderTest.php
+++ FinSearchUnified/Tests/Bundle/SearchBundleFindologic/QueryBuilderTest.php
@@ -4,24 +4,44 @@ namespace FinSearchUnified\Tests\Bundle\SearchBundleFindologic;
 
 use Exception;
 use FinSearchUnified\Bundle\SearchBundleFindologic\QueryBuilder;
+use Shopware\Bundle\PluginInstallerBundle\Service\InstallerService;
 use Shopware\Components\HttpClient\GuzzleHttpClient;
 use Shopware\Components\HttpClient\RequestException;
 use Shopware\Components\HttpClient\Response;
 use Shopware\Components\Test\Plugin\TestCase;
+use Shopware_Components_Config;
 
 class QueryBuilderTest extends TestCase
 {
+    /**
+     * @var Shopware_Components_Config
+     */
+    private $config;
+
+    /**
+     * @var InstallerService
+     */
+    private $installerService;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->config = Shopware()->Container()->get('config');
+        $this->installerService = Shopware()->Container()->get('shopware_plugininstaller.plugin_manager');
+    }
+
     /**
      * Data provider for testing scenarios of findologic request execution
      *
      * @return array
      */
-    public function querybuilderResponseProvider()
+    public function aliveResponseProvider()
     {
         return [
-            'response is successful and body contains "alive"' => [200, 'alive', 'alive'],
-            'response is not successful and body is empty' => [404, '', null],
-            'response is successful and body is empty' => [200, '', null]
+            'Response is successful and body contains "alive"' => [200, 'alive', 2, 'alive'],
+            'Response is not successful and body is empty' => [404, '', 1, null],
+            'Response is successful and body is empty' => [200, '', 1, null]
         ];
     }
 
@@ -50,7 +70,6 @@ class QueryBuilderTest extends TestCase
      */
     public function testExceptionInFindologicRequest($responseCode, $responseBody, $expectedIsAlive, $expectedExecute)
     {
-        $config = Shopware()->Container()->get('config');
         $httpClientMock = $this->createMock(GuzzleHttpClient::class);
 
         $httpResponse = new Response($responseCode, [], $responseBody);
@@ -60,46 +79,41 @@ class QueryBuilderTest extends TestCase
                 ->method('get')
                 ->will($this->throwException(new Exception()));
         } else {
-            $httpClientMock->expects($this->at(0))
+            $httpClientMock->expects($this->exactly(2))
                 ->method('get')
-                ->willReturn($httpResponse);
-            $httpClientMock->expects($this->at(1))
-                ->method('get')
-                ->will($this->throwException(new Exception()));
-            $httpClientMock->expects($this->exactly(2))->method('get');
+                ->will($this->onConsecutiveCalls($httpResponse, $this->throwException(new Exception())));
         }
         $querybuilder = new QueryBuilder(
             $httpClientMock,
-            Shopware()->Container()->get('shopware_plugininstaller.plugin_manager'),
-            $config
+            $this->installerService,
+            $this->config
         );
         $response = $querybuilder->execute();
         $this->assertSame($expectedExecute, $response);
     }
 
     /**
-     * @dataProvider querybuilderResponseProvider
+     * @dataProvider aliveResponseProvider
      *
      * @param int $responseCode
-     * @param string|RequestException $responseBody
+     * @param string $responseBody
+     * @param int $callCount
      * @param string|null $expectedExecute
      *
      * @throws Exception
      */
-    public function testExecutionOfFindologicRequest($responseCode, $responseBody, $expectedExecute)
+    public function testAliveResponseForFindologicRequest($responseCode, $responseBody, $callCount, $expectedExecute)
     {
-        $config = Shopware()->Container()->get('config');
-        $httpClientMock = $this->createMock(GuzzleHttpClient::class);
-
         $httpResponse = new Response($responseCode, [], $responseBody);
-        $httpClientMock->expects($this->atLeastOnce())
+        $httpClientMock = $this->createMock(GuzzleHttpClient::class);
+        $httpClientMock->expects($this->exactly($callCount))
             ->method('get')
             ->willReturn($httpResponse);
 
         $querybuilder = new QueryBuilder(
             $httpClientMock,
-            Shopware()->Container()->get('shopware_plugininstaller.plugin_manager'),
-            $config
+            $this->installerService,
+            $this->config
         );
         $response = $querybuilder->execute();
         $this->assertSame($expectedExecute, $response);
@@ -108,8 +122,8 @@ class QueryBuilderTest extends TestCase
     public function querybuilderRequestUrlProvider()
     {
         return [
-            '"execute" uses SEARCH_ENDPOINT' => [true, QueryBuilder::SEARCH_ENDPOINT],
-            '"execute" uses NAVIGATION_ENDPOINT' => [false, QueryBuilder::NAVIGATION_ENDPOINT],
+            'Request Url uses SEARCH_ENDPOINT' => [true, QueryBuilder::SEARCH_ENDPOINT],
+            'Request Url uses NAVIGATION_ENDPOINT' => [false, QueryBuilder::NAVIGATION_ENDPOINT],
         ];
     }
 
@@ -123,15 +137,13 @@ class QueryBuilderTest extends TestCase
      */
     public function testQuerybuilderRequestUrl($isSearch, $endpoint)
     {
-        $config = Shopware()->Container()->get('config');
-        $installerService = Shopware()->Container()->get('shopware_plugininstaller.plugin_manager');
-        $plugin = $installerService->getPluginByName('FinSearchUnified');
+        $plugin = $this->installerService->getPluginByName('FinSearchUnified');
         $shopUrl = rtrim(Shopware()->Shop()->getHost(), '/');
         $parameters = [
             'outputAdapter' => 'XML_2.0',
             'userip' => 'UNKNOWN',
             'revision' => $plugin->getVersion(),
-            'shopkey' => $config->offsetGet('ShopKey')
+            'shopkey' => $this->config->offsetGet('ShopKey')
         ];
 
         $aliveUrl = sprintf(
@@ -149,31 +161,26 @@ class QueryBuilderTest extends TestCase
             http_build_query($parameters)
         );
 
-        $httpClientMock = $this->createMock(GuzzleHttpClient::class);
-
         $httpResponse = new Response(200, [], 'alive');
-        $httpClientMock->expects($this->at(0))
+        $httpClientMock = $this->createMock(GuzzleHttpClient::class);
+        $aliveCallback = $this->returnCallback(function ($url) use ($aliveUrl, $httpResponse) {
+            \PHPUnit_Framework_Assert::assertSame($aliveUrl, $url);
+
+            return $httpResponse;
+        });
+        $executeCallback = $this->returnCallback(function ($url) use ($executeUrl, $httpResponse) {
+            \PHPUnit_Framework_Assert::assertSame($executeUrl, $url);
+
+            return $httpResponse;
+        });
+        $httpClientMock->expects($this->exactly(2))
             ->method('get')
-            ->will($this->returnCallback(function ($url) use ($aliveUrl, $httpResponse) {
-                \PHPUnit_Framework_Assert::assertSame($aliveUrl, $url);
-
-                return $httpResponse;
-            }));
-
-        $httpClientMock->expects($this->at(1))
-            ->method('get')
-            ->will($this->returnCallback(function ($url) use ($executeUrl, $httpResponse) {
-                \PHPUnit_Framework_Assert::assertSame($executeUrl, $url);
-
-                return $httpResponse;
-            }));
-
-        $httpClientMock->expects($this->exactly(2))->method('get');
+            ->will($this->onConsecutiveCalls($aliveCallback, $executeCallback));
 
         $querybuilder = new QueryBuilder(
             $httpClientMock,
-            $installerService,
-            $config
+            $this->installerService,
+            $this->config
         );
 
         $querybuilder->execute($isSearch);
