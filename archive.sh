8842fd51a99ec6c8bb02daca4c4d82b9dc2e63a4
Ali Zaidi <zep-com@users.noreply.github.com>
2019-05-13 12:42:56 +0500

    SW-263 dont add local changes to archive (#216)
    * SW-263 dont add local changes to archive
    
    * Review changes
    
    * Review changes
    
    * Removed else block

--- archive.sh
+++ archive.sh
@@ -11,6 +11,26 @@ VERSION_RAW="$(sed -n 's|<version>\(.*\)</version>|\1|p' ./FinSearchUnified/plug
 VERSION="$(echo -e "${VERSION_RAW}" | tr -d '[:space:]')"
 echo "Version: ${VERSION}"
 
+STASH=$(git stash)
+
+echo ${STASH}
+
+git fetch --all --tags --prune
+
+TAG=$(git tag -l "v${VERSION}")
+
+# Get current working branch
+BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+
+# If tag is available we proceed with the checkout and zip commands
+# else exit with code 1
+if [[ -z "${TAG}" ]]; then
+    echo "[ERROR]: Tag ${TAG} not found"
+    exit 1
+fi
+
+git checkout tags/${TAG}
+
 # Copying plugins files
 echo "Copying files ... "
 cp -rf ./FinSearchUnified/ /tmp/FinSearchUnified
@@ -24,7 +44,19 @@ composer install --no-dev
 cd /tmp
 
 # Run archive command to create the zip in the root directory
-zip -r9 ${ROOT_DIR}/FinSearchUnified-${VERSION}.zip FinSearchUnified -x FinSearchUnified/phpunit.xml.dist FinSearchUnified/shopware-phpcs.xml FinSearchUnified/Tests/\*
+zip -r9 ${ROOT_DIR}/FinSearchUnified-${VERSION}.zip FinSearchUnified -x FinSearchUnified/phpunit.xml.dist \
+FinSearchUnified/shopware-phpcs.xml FinSearchUnified/Tests/\*
 
 # Delete the directory after script execution
 rm -rf "/tmp/FinSearchUnified"
+
+cd ${ROOT_DIR}
+
+echo "Restoring work in progress ... "
+
+git checkout ${BRANCH}
+
+# Only apply stash if there are some local changes
+if [[ ${STASH} != "No local changes to save" ]]; then
+    git stash pop
+fi
