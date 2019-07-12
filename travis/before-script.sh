a3f721c0448bef58d961e6b1d5dab4d85c4ce282
Huzaifa <huzaifah.mustafa@gmail.com>
2019-07-12 18:00:41 +0500

    env variable changes

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -4,14 +4,15 @@ set -ev
 printenv
 
 PLUGIN_NAME=FinSearchUnified
-SHOPWARE_DIRECTORY="${HOME}"/shopware
+SHOPWARE_DIRECTORY=${HOME}/shopware
 PLUGIN_DIRECTORY=${SHOPWARE_DIRECTORY}/custom/plugins
 
-if [[ ${TRAVIS_BUILD_STAGE_NAME} != 'Lint' ]]; then
+if [[ ${TRAVIS_BUILD_STAGE_NAME} != "Lint" ]]; then
     if [[ "$(php --version | grep -cim1 xdebug)" -ge 1 ]]; then phpenv config-rm xdebug.ini; fi
-    git clone https://github.com/shopware/shopware.git ${SHOPWARE_DIRECTORY} --branch "$(SHOPWARE_VERSION)"
+
+    git clone https://github.com/shopware/shopware.git ${SHOPWARE_DIRECTORY} --branch ${SHOPWARE_VERSION}
     ant -f ${SHOPWARE_DIRECTORY}/build/build.xml -Dapp.host=localhost -Ddb.user=travis -Ddb.host=127.0.0.1 -Ddb.name=shopware build-unit
-    mv "$(TRAVIS_BUILD_DIR)/${PLUGIN_NAME}" ${PLUGIN_DIRECTORY}/${PLUGIN_NAME}
+    mv ${TRAVIS_BUILD_DIR}/${PLUGIN_NAME} ${PLUGIN_DIRECTORY}/${PLUGIN_NAME}
     php ${SHOPWARE_DIRECTORY}/bin/console sw:plugin:refresh
     php ${SHOPWARE_DIRECTORY}/bin/console sw:plugin:install ${PLUGIN_NAME}
     php ${SHOPWARE_DIRECTORY}/bin/console sw:plugin:activate ${PLUGIN_NAME}
