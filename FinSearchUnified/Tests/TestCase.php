f40dc523e8d696861bab13fbb01e12d633d722c2
Ismail Hanli <i.hanli@findologic.com>
2019-07-17 09:34:08 +0200

    Fix method declarations

--- FinSearchUnified/Tests/TestCase.php
+++ FinSearchUnified/Tests/TestCase.php
@@ -60,7 +60,7 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
      */
     private static $pluginStates = [];
 
-    public static function setUpBeforeClass()
+    public static function setUpBeforeClass():void
     {
         self::$pluginManager = Shopware()->Container()->get('shopware_plugininstaller.plugin_manager');
         $loadedPlugins = static::$ensureLoadedPlugins;
@@ -78,7 +78,7 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
         }
     }
 
-    public static function tearDownAfterClass()
+    public static function tearDownAfterClass():void
     {
         self::restorePluginStates();
         self::$pluginManager = null;
