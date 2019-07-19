2420dc67997471c006f1e2c4149aaefcb21f7adc
Huzaifa <huzaifah.mustafa@gmail.com>
2019-07-19 18:20:01 +0500

    SW-281-facet-handler-image-filter (1 test remaining)

--- FinSearchUnified/Bundle/SearchBundleFindologic/FacetHandler/ImageFacetHandler.php
+++ FinSearchUnified/Bundle/SearchBundleFindologic/FacetHandler/ImageFacetHandler.php
@@ -3,15 +3,21 @@
 namespace FinSearchUnified\Bundle\SearchBundleFindologic\FacetHandler;
 
 use FinSearchUnified\Bundle\SearchBundleFindologic\PartialFacetHandlerInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Event\CompleteEvent;
+use GuzzleHttp\Pool;
 use Shopware\Bundle\SearchBundle\Criteria;
 use Shopware\Bundle\SearchBundle\FacetInterface;
+use Shopware\Bundle\SearchBundle\FacetResult\MediaListFacetResult;
+use Shopware\Bundle\SearchBundle\FacetResult\MediaListItem;
+use Shopware\Bundle\StoreFrontBundle\Struct\Media;
 use Shopware\Components\HttpClient\GuzzleFactory;
 use SimpleXMLElement;
 
 class ImageFacetHandler implements PartialFacetHandlerInterface
 {
     /**
-     * @var \GuzzleHttp\ClientInterface
+     * @var ClientInterface
      */
     private $guzzleClient;
 
@@ -24,11 +30,43 @@ class ImageFacetHandler implements PartialFacetHandlerInterface
         $this->guzzleClient = $guzzleFactory->createClient($guzzleConfig);
     }
 
+    /**
+     * @param FacetInterface $facet
+     * @param Criteria $criteria
+     * @param SimpleXMLElement $filter
+     *
+     * @return MediaListFacetResult
+     */
     public function generatePartialFacet(FacetInterface $facet, Criteria $criteria, SimpleXMLElement $filter)
     {
-        // TODO: Implement generatePartialFacet() method.
+        $actives = [];
+
+        $condition = $criteria->getCondition($facet->getName());
+
+        if ($condition !== null) {
+            $actives = $condition->getValue();
+        }
+
+        if (!is_array($actives)) {
+            $actives = [$actives];
+        }
+
+        $values = $this->getMediaListItems($actives, $filter->items->item);
+
+        return new MediaListFacetResult(
+            $facet->getName(),
+            $criteria->hasCondition($facet->getName()),
+            $facet->getLabel(),
+            $values,
+            $facet->getFormFieldName()
+        );
     }
 
+    /**
+     * @param SimpleXMLElement $filter
+     *
+     * @return bool
+     */
     public function supportsFilter(SimpleXMLElement $filter)
     {
         $type = (string)$filter->type;
@@ -36,8 +74,74 @@ class ImageFacetHandler implements PartialFacetHandlerInterface
         return ($type === 'image' || ($type === 'color' && $filter->items->item[0]->image));
     }
 
-    private function getMediaListItems()
+    /**
+     * @param array $active
+     * @param SimpleXMLElement $filterItems
+     *
+     * @return array
+     */
+    private function getMediaListItems(array $active, SimpleXMLElement $filterItems)
     {
+        $listItems = [];
+        $items = [];
+        $requests = [];
 
+        foreach ($filterItems as $filterItem) {
+
+            $name = (string)$filterItem->name;
+            $isActive = array_search($name, $active) !== false;
+
+            if (empty($filterItem->image)) {
+
+                $listItems[] = new MediaListItem(
+                    $name,
+                    $name,
+                    $isActive
+                );
+            } else {
+
+                $url = (string)$filterItem->image;
+
+                $items[$url] = [
+                    'name' => $name,
+                    'active' => $isActive
+                ];
+
+                $requests[] = $this->guzzleClient->createRequest('HEAD', $url);
+
+                $options = [
+                    'complete' => function (CompleteEvent $event) use ($items) {
+
+                        $url = $event->getRequest()->getUrl();
+                        $data = $items[$url];
+
+                        $media = new Media();
+                        $media->setFile($url);
+
+                        $listItems[] = new MediaListItem(
+                            $data['name'],
+                            $data['name'],
+                            $data['active'],
+                            $media
+                        );
+                    },
+                    'error' => function (CompleteEvent $event) use ($items) {
+
+                        $url = $event->getRequest()->getUrl();
+                        $data = $items[$url];
+
+                        $listItems[] = new MediaListItem(
+                            $data['name'],
+                            $data['name'],
+                            $data['active']
+                        );
+                    }
+                ];
+
+                Pool::send($this->guzzleClient, $requests, $options);
+            }
+        }
+
+        return $listItems;
     }
 }
