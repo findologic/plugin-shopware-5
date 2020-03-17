2067f7cc72e81f4c0ff1fe67c0b277e0abb4ec8c
tobiasgraml11 <t.graml@findologic.com>
2020-03-17 11:33:36 +0100

    Nevermind

--- FinSearchUnified/Bundle/SearchBundleFindologic/FacetHandler/RangeFacetHandler.php
+++ FinSearchUnified/Bundle/SearchBundleFindologic/FacetHandler/RangeFacetHandler.php
@@ -67,7 +67,7 @@ class RangeFacetHandler implements PartialFacetHandlerInterface
     private function getUnit(SimpleXMLElement $filter)
     {
         $shopwareVersion = Shopware()->Config()->get('version');
-        if (version_compare($shopwareVersion, '5.3.0', '<') === -1) {
+        if (version_compare($shopwareVersion, '5.3.0', '<')) {
             // Shopware >5.3.0 does not support units. In Shopware 5.2.x this argument is the template path.
             return self::TEMPLATE_PATH;
         }
