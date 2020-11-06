6380e9674987f331a875ed0640fff4493202715a
Tobias Graml <t.graml@findologic.com>
2020-11-06 10:00:40 +0100

    Add ltrim

--- FinSearchUnified/BusinessLogic/Models/FindologicArticleModel.php
+++ FinSearchUnified/BusinessLogic/Models/FindologicArticleModel.php
@@ -530,13 +530,9 @@ class FindologicArticleModel
 
             $catPath = $this->seoRouter->sCategoryPath($category->getId());
 
-            var_dump($catPath);
-
             foreach ($catPath as $key => $item) {
                 $catPath[$key] = str_replace('/', '', $item);
             }
-
-            var_dump($catPath);
 
             while (!empty($catPath)) {
                 $tempPath = sprintf($routerCategoryTemplate, implode('/', $catPath));
@@ -546,7 +542,7 @@ class FindologicArticleModel
                 }
 
                 if (!StaticHelper::isEmpty($tempPath)) {
-                    $categoryPath = $this->seoRouter->sCleanupPath($tempPath);
+                    $categoryPath = '/' . ltrim($this->seoRouter->sCleanupPath($tempPath), '/');
                     $catUrlArray[] = $categoryPath;
                     $catUrlArray[] = Shopware()->Shop()->getBaseUrl() . $categoryPath;
                 }
