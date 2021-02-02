f82abf37b8309f775ed9b1d45e2cdbce98e6b35a
Dominik Brader <d.brader@findologic.com>
2021-02-02 15:15:44 +0100

    SW-568_switch_to_github_actions (#336)
    Our internal build pipeline now runs on GitHub Actions instead of Travis.

--- FinSearchUnified/Tests/Bundle/ProductNumberSearchTest.php
+++ FinSearchUnified/Tests/Bundle/ProductNumberSearchTest.php
@@ -67,7 +67,7 @@ class ProductNumberSearchTest extends TestCase
             ->willReturnMap($configArray);
         $mockConfig->expects($this->any())
             ->method('get')
-            ->willReturn(!empty(getenv('SHOPWARE_VERSION')) ? getenv('SHOPWARE_VERSION') : '5.6.4');
+            ->willReturn(StaticHelper::getShopwareVersion());
 
         Shopware()->Container()->set('config', $mockConfig);
 
--- FinSearchUnified/Tests/Bundle/SearchBundleFindologic/FacetHandler/RangeFacetHandlerTest.php
+++ FinSearchUnified/Tests/Bundle/SearchBundleFindologic/FacetHandler/RangeFacetHandlerTest.php
@@ -33,7 +33,7 @@ class RangeFacetHandlerTest extends TestCase
             ->getMock();
         $mockConfig->expects($this->any())
             ->method('get')
-            ->willReturn(!empty(getenv('SHOPWARE_VERSION')) ? getenv('SHOPWARE_VERSION') : '5.6.4');
+            ->willReturn(StaticHelper::getShopwareVersion());
 
         Shopware()->Container()->set('config', $mockConfig);
     }
