0885e51714f56ee4c723dc3479ac5144d14c44d8
Tobias Graml <t.graml@findologic.com>
2021-08-06 08:59:01 +0200

    SW-637 Range values are not considered for filtering  (#347)
    Fixed a bug that would cause range-slider filters to not properly filter the results.

--- FinSearchUnified/Tests/Bundle/SearchBundleFindologic/QueryBuilder/SearchQueryBuilderTest.php
+++ FinSearchUnified/Tests/Bundle/SearchBundleFindologic/QueryBuilder/SearchQueryBuilderTest.php
@@ -98,8 +98,8 @@ class SearchQueryBuilderTest extends TestCase
         $params = $searchNavigationRequest->getParams();
         $this->assertArrayHasKey('attrib', $params);
         $this->assertArrayHasKey('price', $params['attrib']);
-        $this->assertEquals(0.44, $params['attrib']['price']['min']);
-        $this->assertEquals(12.00, $params['attrib']['price']['max']);
+        $this->assertSame('0.44', $params['attrib']['price']['min']);
+        $this->assertSame('12', $params['attrib']['price']['max']);
     }
 
     public function testFilterParameter()
