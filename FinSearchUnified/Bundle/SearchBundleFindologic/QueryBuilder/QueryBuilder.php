f6e792d33cb6481bbf9c2a968b5b9a394b0de310
Nenad Jovanovic <nenad.jovanovic@soprex.com>
2022-05-09 16:27:04 +0200

    SW-602 Lint error fix

--- FinSearchUnified/Bundle/SearchBundleFindologic/QueryBuilder/QueryBuilder.php
+++ FinSearchUnified/Bundle/SearchBundleFindologic/QueryBuilder/QueryBuilder.php
@@ -232,7 +232,11 @@ abstract class QueryBuilder
         $this->searchNavigationRequest->setRevision(rtrim($plugin->getVersion()));
         $this->searchNavigationRequest->setOutputAdapter(OutputAdapter::XML_21);
         $this->searchNavigationRequest->addIndividualParam('shopType', 'Shopware5', Request::ADD_VALUE);
-        $this->searchNavigationRequest->addIndividualParam('shopVersion', Shopware()->Config()->offsetGet('version'), Request::ADD_VALUE);
+        $this->searchNavigationRequest->addIndividualParam(
+            'shopVersion',
+            Shopware()->Config()->offsetGet('version'),
+            Request::ADD_VALUE
+        );
     }
 
     /**
