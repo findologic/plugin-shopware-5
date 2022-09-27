ce1b2b3a3d5a23106bae3ebc4181e85f08650699
Bojan <bojan.dimitrijevic@soprex.com>
2022-09-27 14:54:19 +0200

    Testing the new changes on github PHP unit tests..

--- FinSearchUnified/Tests/Bundle/SearchBundleFindologic/QueryBuilder/SearchQueryBuilderTest.php
+++ FinSearchUnified/Tests/Bundle/SearchBundleFindologic/QueryBuilder/SearchQueryBuilderTest.php
@@ -13,7 +13,7 @@ use FinSearchUnified\Tests\Helper\Utility;
 use FinSearchUnified\Tests\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
 use Shopware\Bundle\PluginInstallerBundle\Service\InstallerService;
-use Shopware_Components_Config;
+use Shopware_Components_Config as Config;
 
 class SearchQueryBuilderTest extends TestCase
 {
@@ -22,19 +22,25 @@ class SearchQueryBuilderTest extends TestCase
      */
     private $installerService;
 
-    /**
-     * @var Shopware_Components_Config
-     */
-    private $config;
-
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->installerService = Shopware()->Container()->get('shopware.plugin_manager');
-        $this->config = Shopware()->Config();
-        $this->config->ShopKey = 'ABCDABCDABCDABCDABCDABCDABCDABCD';
-        $this->config->version = '5.7.7';
+        // Create mock object for Shopware Config and explicitly return the values
+        $mockConfig = $this->getMockBuilder(Config::class)
+            ->setMethods(['getByNamespace', 'get'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $mockConfig->expects($this->any())
+            ->method('getByNamespace')
+            ->with('FinSearchUnified', 'ShopKey', null)
+            ->willReturn('ABCDABCDABCDABCDABCDABCDABCDABCD');
+        $mockConfig->expects($this->any())
+            ->method('get')
+            ->willReturn(StaticHelper::getShopwareVersion());
+
+        Shopware()->Container()->set('config', $mockConfig);
 
         // Set default values for test
         $_SERVER['REMOTE_ADDR'] = '192.168.1.1';
@@ -58,7 +64,7 @@ class SearchQueryBuilderTest extends TestCase
         );
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             $apiClientMock,
             $searchNavigationRequest
         );
@@ -72,7 +78,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -89,7 +95,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -108,7 +114,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -127,7 +133,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -163,7 +169,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -181,7 +187,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new NavigationRequest();
         $queryBuilder = new NavigationQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -200,7 +206,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -217,7 +223,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -234,7 +240,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -251,7 +257,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -271,7 +277,7 @@ class SearchQueryBuilderTest extends TestCase
 
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -300,13 +306,13 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
 
         $usergrouphash = StaticHelper::calculateUsergroupHash(
-            $this->config->getByNamespace('FinSearchUnified', 'ShopKey'),
+            Shopware()->Config()->getByNamespace('FinSearchUnified', 'ShopKey'),
             $userGroup
         );
         $queryBuilder->addUserGroup($userGroup);
@@ -353,7 +359,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -371,7 +377,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         $queryBuilder = new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -390,7 +396,7 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
@@ -405,13 +411,14 @@ class SearchQueryBuilderTest extends TestCase
         $searchNavigationRequest = new SearchRequest();
         new SearchQueryBuilder(
             $this->installerService,
-            $this->config,
+            Shopware()->Config(),
             null,
             $searchNavigationRequest
         );
 
         $params = $searchNavigationRequest->getParams();
         $this->assertArrayHasKey('shopVersion', $params);
+        dd($params);
         $this->assertSame('5.7.7', $params['shopVersion']);
     }
 }
